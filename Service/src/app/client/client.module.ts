5f4a2f7a4ca18c6705c81013d0023d8ce56a832b
soham1209 <sohamns1234@gmail.com>
2024-11-22 19:20:23 +0530

    fifth commit

--- Service/src/app/client/client.module.ts
+++ Service/src/app/client/client.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
 import { ClientDashboardComponent } from './pages/client-dashboard/client-dashboard.component';
+import { DemoNgZorroAntdModule } from '../DemoNgZorroAntdModule';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +15,10 @@ import { ClientDashboardComponent } from './pages/client-dashboard/client-dashbo
   ],
   imports: [
     CommonModule,
-    ClientRoutingModule
+    ClientRoutingModule,
+    DemoNgZorroAntdModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ClientModule { }
---
+++ Service/src/app/client/services/client.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserServiceService } from 'src/app/basic/services/storage/user-service.service';
+
+const BASE_URL = 'http://localhost:8080/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllAds(): Observable<any> {
+   
+    return this.http.get(BASE_URL + `api/client/ads`, {
+      headers: this.createAuthorizationHeader()
+    })
+  }
+
+  searchAdByName(name:any): Observable<any> {
+    return this.http.get(BASE_URL + `api/client/search/${name}`, {
+      headers: this.createAuthorizationHeader()
+    })
+  }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization',
+      'Bearer ' + UserServiceService.getToken()
+    )
+  }
+}
