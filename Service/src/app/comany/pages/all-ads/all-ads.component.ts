1f507e6b30437e1848de486dda093e618f10c05f
soham1209 <sohamns1234@gmail.com>
2024-11-30 16:27:07 +0530

    design 3

--- Service/src/app/comany/pages/all-ads/all-ads.component.ts
+++ Service/src/app/comany/pages/all-ads/all-ads.component.ts
@@ -13,6 +13,18 @@ export class AllAdsComponent {
   constructor(private companyService: CompanyService,
     private notification: NzNotificationService,
   ) { }
+
+  getCardBackground(index: number): string {
+    return index % 2 === 0 ? '#06BEE1' : '#2541B2'; // Blue for even index, Light blue for odd index
+  }
+  getUpdateButtonColor(backgroundColor: string): string {
+    if (backgroundColor === '#2541B2') {
+      return '#06BEE1'; // Opposite of #2541B2
+    } else if (backgroundColor === '#06BEE1') {
+      return '#2541B2'; // Opposite of #06BEE1
+    }
+    return '#000000'; // Default color if not matched
+  }
   ngOnInit() {
     this.getAllAdsByUserId();
   }
